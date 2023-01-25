bbb69ab7eec2f8e5fd433ebdfb71d865f54fea4f
Ahmet Demir <mumaru171@gmail.com>
2023-01-25 23:10:56 +0300

    frontend is generated

---
+++ frontend/ecofront/src/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter,Routes,Route,Link } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+
+
+const App = () => {
+  return (
+    <>
+     <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Detail" element={<Detail />} />
+      </Routes>
+     </BrowserRouter>
+     
+    </>
+  );
+}
+
+export default App;
