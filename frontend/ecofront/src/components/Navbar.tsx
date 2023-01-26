e3cda536e6bda181be88005a6fea407989b212dc
Ahmet Demir <mumaru171@gmail.com>
2023-01-26 21:42:37 +0300

    Redux entagration is ok

--- frontend/ecofront/src/components/Navbar.tsx
+++ frontend/ecofront/src/components/Navbar.tsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { BsLightbulb, BsBasketFill,BsMoonStarsFill } from 'react-icons/bs'
 
 const Navbar = () => {
+
+  const [color,setColor] = useState(false);
+  useEffect(()=>{
+    const root: HTMLElement = document.getElementById('root') as HTMLElement;
+    if(color){
+      root.style.backgroundColor = "black";
+      root.style.color = "#EBEBEB";
+
+    }else{
+      root.style.backgroundColor = "white";
+      root.style.color = "black";
+    }
+  },[color])
+
   return (
-    <div>Navbar</div>
+    <div className="flex items-center justify-between px-3 h-24">
+      <div className="text-2xl font-bold tracking-wider">Logo</div>
+      <div className="flex items-center space-x-4">
+        <input type="text" placeholder='Ara' className="border p-3 outline-none rounded-lg" />
+        <div onClick={()=>setColor(!color)} >
+          {
+            color ? <BsMoonStarsFill size={25} className="cursor-pointer" /> : <BsLightbulb size={25} className="cursor-pointer" /> 
+          }
+          
+        </div>
+
+        <div className='relative'>
+          <BsBasketFill size={25} className="cursor-pointer" />
+          <span className="absolute -top-1 -right-1 px-1 bg-red-600 text-white rounded-full text-sm">3</span>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
