6a821ba5ffdd65f6ff3fc7ca0f66b6a135dc5ef1
Ahmet Demir <mumaru171@gmail.com>
2023-02-06 19:30:22 +0300

    its ok

--- frontend/ecofront/src/redux/actions/products.tsx
+++ frontend/ecofront/src/redux/actions/products.tsx
@@ -1,13 +1,13 @@
 import { Dispatch } from "react";
-import { AnyAction } from "redux";
 import store from "../store";
 
 
-export const productAction :Dispatch<AnyAction> = () => async () => {
+export const productAction = async ():Dispatch<any> => {
     const data = await fetch('https://fakestoreapi.com/products')
         .then(res => res.json());
         
     console.log("Products tan : \n----------------------------------------------------------\n" + data + "\n---------------------------------------------------------\n");
         
-        store.dispatch({type:"GET_PRODUCTS",payload:data});
-}
+       let x :any = await store.dispatch({type:"GET_PRODUCTS",payload:data});
+        return x;
+    }
