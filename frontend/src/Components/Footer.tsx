2380740f0b18abfb42b299a61b98e7d996a27bee
Ahmet Demir <mumaru171@gmail.com>
2023-02-09 19:46:20 +0300

    Footer and Navbae companent generated

---
+++ frontend/src/Components/Footer.tsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+    return (
+        <h1 className="bold text-blue-400">
+            Footer
+        </h1>
+    );
+}
+
+export default Footer;
---
+++ frontend/src/Components/Navbar.tsx
@@ -0,0 +1,12 @@
+
+
+const Navbar = () => {
+    return (
+        <h1 className="bold text-center text-gray-500">
+            Navbar
+        </h1>
+    );
+}
+
+
+export default Navbar;
