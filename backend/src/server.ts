1044d31b31538f5de5d70fca2ec02e08ee53d236
Ahmet Demir <mumaru171@gmail.com>
2023-05-19 23:40:14 +0300

    descs deleted

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,23 +1,15 @@
 
-//#region Imports
+
 import  {App}  from '../src/App';
 import {DbConnection} from '../src/dataaccess/MongoSetting';
 require('dotenv').config();
 
-//#endregion
 
 
-//#region Constructers
 
 new DbConnection();
 
 App.set("PORT",process.env.PORT)
 
-//#endregion
-
-//#region Middlewares and Routes
-
 
 App.listen(App.get("PORT"),()=>{console.log("The server started , Port is : " + App.get("PORT") );});
-
-//#endregion
--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useId } from 'react'
+
 
 const Home = () => {
+
+  let id = useId();
+
+  console.log( "Deger" + id);
+
   return (
     <section>
         Ana Safa
+        <h1> Id :  {id} </h1>
     </section>
   )
 }
